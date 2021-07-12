e387d0874d43e3e98c8ebcd153eda033b24713c2
unique-chan <yechankim@gm.gist.ac.kr>
2021-07-12 15:53:36 +0900

    loader.py impl

--- code/loader.py
+++ code/loader.py
@@ -1,10 +1,23 @@
 from tensorflow import keras
-from parser import parse_args
 
 
-def train_valid_test(data_dir, args, train='train', valid='valid', test='test'):
+def train_test(args, train='train', test='test'):
+    train_dir, test_dir =\
+        '{}/{}'.format(args.data, train), '{}/{}'.format(args.data, test)
+    train_dataset = keras.preprocessing.image_dataset_from_directory(directory=train_dir,
+                                                                     batch_size=args.batch_size,
+                                                                     image_size=(args.height, args.width),
+                                                                     shuffle=True)
+    test_dataset = keras.preprocessing.image_dataset_from_directory(directory=test_dir,
+                                                                    batch_size=args.batch_size,
+                                                                    image_size=(args.height, args.width),
+                                                                    shuffle=False)
+    return train_dataset, test_dataset
+
+
+def train_valid_test(args, train='train', valid='valid', test='test'):
     train_dir, valid_dir, test_dir =\
-        '{}/{}'.format(data_dir, train), '{}/{}'.format(data_dir, valid), '{}/{}'.format(data_dir, test)
+        '{}/{}'.format(args.data, train), '{}/{}'.format(args.data, valid), '{}/{}'.format(args.data, test)
     train_dataset = keras.preprocessing.image_dataset_from_directory(directory=train_dir,
                                                                      batch_size=args.batch_size,
                                                                      image_size=(args.height, args.width),
